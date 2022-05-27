4e7aed19661f025177ffbc34bb5975af43df05bc
Paul-Julius Hillmann <pjhillmann@gmail.com>
2022-05-27 17:50:34 +0200

    added change to display of UserIdMappings so that old mappings are removed when other chats are selected to donate

--- javascripts/showUserIdMapping.js
+++ javascripts/showUserIdMapping.js
@@ -1,10 +1,20 @@
 function showUserIdMapping(userIdMapping) {
 
-    Object.entries(userIdMapping).forEach((mapping) => {
-        if (mapping[0] !== "System") {
-            $("#display-userIDMapping").append("<p>" + mapping[0] + " &rarr; " + mapping[1] + "</p>")
-        }
+    clearPreviousRenderedMappings()
+        .then(() => {
+        Object.entries(userIdMapping).forEach((mapping) => {
+            if (mapping[0] !== "System") {
+                $("#display-userIDMapping").append("<p class='mapping-item'>" + mapping[0] + " &rarr; " + mapping[1] + "</p>")
+            }
+        })
     })
 }
+
+function clearPreviousRenderedMappings() {
+    return new Promise((resolve) => {
+        $("#display-userIDMapping .mapping-item").remove();
+        resolve();
+    });
+};
 
 module.exports = showUserIdMapping;
