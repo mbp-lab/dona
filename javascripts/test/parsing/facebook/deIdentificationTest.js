fd7076222e05e7d76909ea333e3473aa41278c09
Paul-Julius Hillmann <pjhillmann@gmail.com>
2022-07-28 12:38:16 +0200

    changed test, return object of method changed -> so test needed to change as well

--- javascripts/test/parsing/facebook/deIdentificationTest.js
+++ javascripts/test/parsing/facebook/deIdentificationTest.js
@@ -14,7 +14,7 @@ describe('DeIdentifying a zip file', function () {
     const zipFile = fs.readFileSync("javascripts/test/resources/validZip.zip");
     it('should return messages with number of words rather than content', function () {
         return handleFile([zipFile]).then(deIdentifiedJson => {
-            deIdentifiedJson[0].messages.forEach((message) => {
+            deIdentifiedJson["deIdentifiedJsonContents"][0].messages.forEach((message) => {
                 expect(message['word_count']).to.equal(6);
             })
         })
@@ -22,7 +22,7 @@ describe('DeIdentifying a zip file', function () {
     it('should return messages with random IDs instead of sender_name', function () {
         return handleFile([zipFile]).then(deIdentifiedJson => {
         //console.log(deIdentifiedJson[0].participants);
-            deIdentifiedJson[0].messages.forEach((message) => {
+            deIdentifiedJson["deIdentifiedJsonContents"][0].messages.forEach((message) => {
                 expect(message.sender_name).to.not.equal('Blah Blah');
                 expect(message.sender_name).to.not.equal('Some Person');
             })
