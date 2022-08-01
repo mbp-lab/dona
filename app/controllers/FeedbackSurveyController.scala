83ba2608436ffc767b9ebf21b64ca3ac4cae6a7d
Jan Bleimling <jbleimling@techfak.uni-bielefeld.de>
2022-08-01 17:12:13 +0200

    Added FeedbackSurveyController.scala

---
+++ app/controllers/FeedbackSurveyController.scala
@@ -0,0 +1,40 @@
+package controllers
+
+import config.FeedbackSurveyConfig
+import javax.inject._
+import models.api._
+import models.domain.DonationDataSourceType.{DonationDataSourceType, _}
+import models.domain.ExternalDonorId
+import org.slf4j.LoggerFactory
+import persistence.DonationService
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.libs.json._
+import play.api.mvc._
+import scalaz.{-\/, \/-}
+import services._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+@Singleton
+final class FeedbackSurveyController @Inject()(
+                                                    feedbackSurveyConfig: FeedbackSurveyConfig,
+                                                    cc: ControllerComponents
+                                                  ) extends AbstractController(cc)
+  with I18nSupport {
+
+  private val logger = LoggerFactory.getLogger(classOf[AbstractController])
+
+  private val GeneratedDonorIdKey = "GeneratedDonorId"
+
+  def goToFeedback: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val isolocaleString = messagesApi.preferred(request).lang.locale.getLanguage
+    for {
+      link = feedbackSurveyConfig.createFeedbackLink(ExternalDonorId(request.session.get(GeneratedDonorIdKey)), isolocaleString).toString
+    } yield {
+      Redirect(link)
+    }
+  }
+
+}
