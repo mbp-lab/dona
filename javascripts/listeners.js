b02252dac8b5acfce1bc72c972e63ac83c980a47
Paul-Julius Hillmann <pjhillmann@gmail.com>
2023-03-13 20:21:55 +0100

    changed texts for chat 1, chat 2, ... in friendsmapping

--- javascripts/listeners.js
+++ javascripts/listeners.js
@@ -246,7 +246,8 @@ function setUpFileHandler() {
 
                 // create data preview and user id mapping
                 renderTable(deIdentifiedJson.deIdentifiedJsonContents, dataSource);
-                renderUserIDMapping(deIdentifiedJson.participantNameToRandomIds, i18nSupport.data('system'), i18nSupport.data('donor'), i18nSupport.data('friend-initial'), dataSource)
+                let contactsPerConv = deIdentifiedJson.deIdentifiedJsonContents.map((conv) => conv.participants)
+                renderUserIDMapping(deIdentifiedJson.participantNameToRandomIds, contactsPerConv, i18nSupport.data('system'), i18nSupport.data('donor'), i18nSupport.data('friend-initial'), i18nSupport.data('and-more-contacts'), i18nSupport.data('chat'),  dataSource)
                 return transformJson(deIdentifiedJson.deIdentifiedJsonContents, donorId, dataSource);
             })
             .then((transformedJson) => {
